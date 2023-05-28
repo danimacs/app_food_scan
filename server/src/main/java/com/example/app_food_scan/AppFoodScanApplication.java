06705918231c800a2a5daeed719364c8aeace7e8
Daniel Macías <dani-mac@hotmail.es>
2023-05-28 17:13:36 +0200

    Initial Commit - Spring

---
+++ server/src/main/java/com/example/app_food_scan/AppFoodScanApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_food_scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFoodScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFoodScanApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/app_food_scan/AppFoodScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_food_scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFoodScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
